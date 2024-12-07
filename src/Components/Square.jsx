7bfebc50e3d0ff38204c3ee347158ed6520033d4
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2024-12-06 16:46:42 -0800

    updating background color

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -3,11 +3,18 @@ import { useControls } from 'leva'
 import { DoubleSide } from 'three'
 
 export default function Square({ position, value, onSquareClick }) {
-  const rotationValues = useControls('square', {
-    rotationX: { value: 0, min: -90, max: 90, step: 0.1 },
-    rotationY: { value: 0, min: -90, max: 90, step: 0.1 },
-    rotationZ: { value: 0, min: -90, max: 90 }
-  })
+  const xPosition = position[0].toFixed(1)
+  const yPosition = position[1].toFixed(1)
+  const zPosition = position[2].toFixed(1)
+  const rotationValues = useControls(
+    'square',
+    {
+      rotationX: { value: 0, min: -90, max: 90, step: 0.1 },
+      rotationY: { value: 0, min: -90, max: 90, step: 0.1 },
+      rotationZ: { value: 0, min: -90, max: 90 }
+    },
+    { collapsed: true }
+  )
   return (
     <>
       <mesh
@@ -17,7 +24,9 @@ export default function Square({ position, value, onSquareClick }) {
         <boxGeometry />
         <meshStandardMaterial color="rebeccapurple" side={DoubleSide} />
       </mesh>
-      {/* <Html position={[0, 2, 0]}>{value}</Html> */}
+      <Html position={[xPosition, yPosition, zPosition]} style={{ color: 'whitesmoke' }}>
+        {value}
+      </Html>
     </>
   )
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -11,13 +11,26 @@ export default function Game() {
       lightPositionY: { value: 7, min: -20, max: 20, step: 0.1 },
       lightPositionZ: { value: 7, min: -20, max: 20, step: 0.1 }
     },
+
+    { collapsed: true }
+  )
+  const { bgColor } = useControls(
+    'scene',
+    {
+      bgColor: {
+        value: '#fda100',
+        label: 'Background Color'
+      }
+    },
     { collapsed: true }
   )
 
   const value = 'x'
   const rows = 3
+
   return (
     <>
+      <color attach="background" args={[bgColor]} />
       <directionalLight position={[lights.lightPositionX, lights.lightPositionY, lights.lightPositionZ]} />
       <Board rows={rows} />
     </>
